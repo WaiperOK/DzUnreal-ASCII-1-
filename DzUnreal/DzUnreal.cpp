e220d12f781843ddec63fdc4ca720973b9e91074
WaiperOK <ruslan.topol2@gmail.com>
2022-07-09 14:44:41 +0300

    Добавьте файлы проекта.

---
+++ DzUnreal/DzUnreal.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    unsigned char q = 'F';
+    cout << q << " " << (int)q << endl;
+    q = 126;      // char можно присвоить и числовое значение
+    cout << q << " " << (int)q << endl;
+    return 0;
+}
